61db8aa24f7659f2220901bb9f95135d9452c550
Arjun Bingly <arjunbin@gmail.com>
2025-02-13 16:33:17 -0500

    Docstrings and Typing

--- src/neuroscidl/eeg/annotator.py
+++ src/neuroscidl/eeg/annotator.py
@@ -2,9 +2,8 @@ import json
 import warnings
 from os import PathLike
 from pathlib import Path
-from typing import Union
+from typing import Union, Optional, List, Tuple, Dict
 
-import mne
 import numpy as np
 import pandas as pd
 from neuroscidl.eeg.utils import hash_df
@@ -12,6 +11,24 @@ from neuroscidl.eeg.utils import hash_df
 warnings.filterwarnings("ignore", module='mne')
 
 class CNTSampleAnnotator:
+    """
+    A class to annotate windowed EEG samples, given a dataframe with column 'n_times'
+    corresponding to the total number of datapoints.
+    Note:
+        file_annotations should contain a column 'n_times' with the number of samples for each file.
+        argument file_annotations can be a path to a csv file or a dataframe,
+        but the path is preferred due to caching and filename resolution.
+
+    Attributes:
+        window_size (int): The size of the window for each sample.
+        window_stride (int): The stride of the window for each sample.
+        window_start (int): The starting index for the window.
+        save_path (Path): The path to save the annotations.
+        save_suffix (str): The suffix to add to the saved annotation files.
+        overwrite (bool): Whether to overwrite existing annotation files.
+        file_annotations (pd.DataFrame): The DataFrame containing file annotations.
+        save_filename (str): The filename to save the annotations.
+    """
     def __init__(self,
                  file_annotations: Union[pd.DataFrame, PathLike],
                  window_size: int= 500,
@@ -20,6 +37,20 @@ class CNTSampleAnnotator:
                  save_path: Union[str, Path] = '.',
                  save_suffix: str = 'sample_annotations',
                  overwrite: bool = False,):
+        """
+        Initializes the CNTSampleAnnotator with the given parameters.
+
+        Args:
+            file_annotations (Union[pd.DataFrame, PathLike]): The path to the CSV file containing the file annotations
+            or the DataFrame itself, path is preferred since the filename is used to generate the save_filename and cache.
+            window_size (int): The size of the window for each sample.
+            window_stride (int): The stride of the window for each sample.
+            window_start (int): The starting index for the window.
+            save_path (Union[str, Path]): The path to save the annotations and config.
+            save_suffix (str): The suffix to add to the saved annotation files.
+            If a DataFrame is passed as file_annotations, the save_filename will be set to this.
+            overwrite (bool): Whether to overwrite existing annotation files, cached files will not be used.
+        """
         self.window_size = window_size
         self.window_stride = window_stride
         self.window_start = window_start
@@ -34,20 +65,13 @@ class CNTSampleAnnotator:
             self.file_annotations = file_annotations
             self.save_filename = self.save_suffix
 
+    def get_config(self) -> Dict[str, Union[int, str]]:
+        """
+        Returns the configuration of the annotator.
 
-    @classmethod
-    def read_cnt(file_path: Union[str, Path], data_format='auto', verbose=False):
-        file_path = Path(file_path)
-        if not file_path.is_file():
-            raise ValueError(f'Path {file_path} is not a file')
-        try:
-            data = mne.io.read_raw_cnt(file_path, preload=True, data_format=data_format, verbose=verbose)
-        except Exception as e:
-            print(f'Error reading file {file_path}: {e}')
-            return None
-        return data
-
-    def get_config(self):
+        Returns:
+            dict: The configuration of the annotator.
+        """
         return {
             'window_size': self.window_size,
             'window_stride': self.window_stride,
@@ -56,20 +80,45 @@ class CNTSampleAnnotator:
         }
 
     @property
-    def config(self):
+    def config(self) -> Dict[str, Union[int, str]]:
+        """
+        Property to get the configuration of the annotator.
+
+        Returns:
+            dict: The configuration of the annotator.
+        """
         return self.get_config()
 
-    def save_config(self):
+    def save_config(self) -> None:
+        """
+        Saves the configuration of the annotator to a JSON file.
+        The JSON file is saved to the save_path as a hidden file with the same name as the annotation file.
+        """
         with open(self.save_path/f'.{self.save_filename}.json', 'w') as f:
             json.dump(self.config, f)
 
-    def get_sample_indices(self, n_samples):
+    def get_sample_indexes(self, n_samples: int) -> List[Tuple[int,int]]:
+        """
+        Returns the start and end indexes for each sample window.
+
+        Args:
+            n_samples (int): The total number of samples.
+
+        Returns:
+            list: A list of tuples containing the start and end indexes for each sample window.
+        """
         start_indices = np.arange(self.window_start, n_samples - self.window_size, self.window_stride)
         end_indices = start_indices + self.window_size
-        return list(zip(list(start_indices), list(end_indices)))
+        return list(zip(start_indices.tolist(), end_indices.tolist()))
 
-    def get_sample_info(self):
-        sample_indices = map(self.get_sample_indices, self.file_annotations['n_times'])
+    def get_sample_info(self) -> pd.DataFrame:
+        """
+        Generates and returns a DataFrame containing the sample information for each window, sample annotations.
+
+        Returns:
+            pd.DataFrame: A DataFrame containing the sample information for each window.
+        """
+        sample_indices = map(self.get_sample_indexes, self.file_annotations['n_times'])
         records = []
         for i, idx  in enumerate(sample_indices):
             _record = self.file_annotations.iloc[i].to_dict()
@@ -81,7 +130,13 @@ class CNTSampleAnnotator:
                 records.append(record)
         return pd.DataFrame.from_records(records)
 
-    def get_cached_annotations(self):
+    def get_cached_annotations(self) -> Optional[pd.DataFrame]:
+        """
+        Returns the cached annotations if they exist and match the current configuration.
+
+        Returns:
+            pd.DataFrame: The cached annotations if they exist and match the current configuration, otherwise None.
+        """
         sample_annotations_config = '.'+self.save_filename + '.json'
         sample_annotations_filename = self.save_filename + '.csv'
         if (self.save_path/sample_annotations_config).exists() and (self.save_path/sample_annotations_filename).exists():
@@ -90,7 +145,13 @@ class CNTSampleAnnotator:
                 if config == self.config:
                     return pd.read_csv(self.save_path/sample_annotations_filename)
 
-    def __call__(self, *args, **kwargs):
+    def __call__(self) -> pd.DataFrame:
+        """
+        Reads or generates and returns the sample annotations.
+
+        Returns:
+            pd.DataFrame: The sample annotations.
+        """
         if not self.overwrite:
             sample_annotations = self.get_cached_annotations()
             if sample_annotations is not None:
