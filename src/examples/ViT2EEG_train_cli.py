f5d77211621a727ccbf04b61f2b723ddcd122011
Arjun Bingly <arjunbin@gmail.com>
2025-02-17 17:05:29 -0500

    Edit cli to make transform_config work with any transform

--- src/examples/ViT2EEG_train_cli.py
+++ src/examples/ViT2EEG_train_cli.py
@@ -53,24 +53,44 @@ class MyLightningCli(LightningCLI):
         print(
             f"{Path(self.config['fit']['data']['annotation_file']).name}:{self.config['fit']['trainer']['logger']['init_args']['tags']}")
 
-    def verify_noise_config_key(self, modify=True):
+    def modify_transform_config(self, transform_config, modify=True):
+        """Modify the transform config to use the same config_key as the annotation_file."""
+        if transform_config.get('init_args') is not None:
+            if transform_config['init_args'].get('config_key') is not None:
+                if transform_config['init_args']['config_key'] != self.config['fit']['data']['annotation_file']:
+                    if modify:
+                        print(
+                            f'Warning: {transform_config['class_path']} config_key is not the same as annotation_file, will be updated.')
+                        transform_config['init_args']['config_key'] = self.config['fit']['data']['annotation_file']
+                    else:
+                        print(
+                            f'Warning: {transform_config['class_path']} config_key is not the same as annotation_file.')
+
+    def verify_transform_config_key(self, modify=True):
         """Validate train_transform config_key is same as annotation_file."""
-        if self.config['fit']['data']['train_transform']['init_args'].get('config_key') is not None:
-            if self.config['fit']['data']['train_transform']['init_args']['config_key'] != self.config['fit']['data'][
-                'annotation_file']:
-                if modify:
-                    print('Warning: train_transform config_key is not the same as annotation_file, will be updated.')
-                    self.config['fit']['data']['train_transform']['init_args']['config_key'] = self.config['fit']['data'][
-                        'annotation_file']
-                else:
-                    print('Warning: train_transform config_key is not the same as annotation_file.')
+        if isinstance(self.config['fit']['data']['train_transform'], list):
+            for transform in self.config['fit']['data']['train_transform']:
+                self.modify_transform_config(transform, modify)
+        else:
+            self.modify_transform_config(self.config['fit']['data']['train_transform'], modify)
 
     def find_noise_config(self):
         """Checks if config for file exists in noise_config.json else create."""
-        noise_config_key = self.config['fit']['data']['train_transform']['init_args']['config_key']
+        noise_config_keys = []
+        if isinstance(self.config['fit']['data']['train_transform'], list):
+            for transform in self.config['fit']['data']['train_transform']:
+                if transform.get('init_args') is not None:
+                    if transform['init_args'].get('config_key') is not None:
+                        noise_config_keys.append(transform['init_args']['config_key'])
+        else:
+            if self.config['fit']['data']['train_transform'].get('init_args') is not None:
+                if self.config['fit']['data']['train_transform']['init_args'].get('config_key') is not None:
+                    noise_config_keys.append(self.config['fit']['data']['train_transform']['init_args']['config_key'])
+
         print('Checking noise config...')
-        calculator = CalculateEEGDist(self.config['fit']['data']['data_dir'])
-        calculator(noise_config_key)
+        for noise_config_key in noise_config_keys:
+            calculator = CalculateEEGDist(self.config['fit']['data']['data_dir'])
+            calculator(noise_config_key)
 
     def before_instantiate_classes(self) -> None:
         """Modify the configuration before instantiating classes."""
@@ -86,14 +106,12 @@ class MyLightningCli(LightningCLI):
             if str(self.config['fit']['trainer']['logger']['class_path']) == 'lightning.pytorch.loggers.MLFlowLogger':
                 self.update_mlflow_tags()
 
-        # Noise config
+        # Transforms config
         if self.config['fit']['data'].get('train_transform') is not None:
-            if self.config['fit']['data']['train_transform'].get('init_args') is not None:
-                # Validate train_transform config_key is same as annotation_file
-                self.verify_noise_config_key(modify=True)
-                # Checks if config for file exists in noise_config.json else create
-                self.find_noise_config()
-
+            # Validate train_transform config_key is same as annotation_file
+            self.verify_transform_config_key(modify=True)
+            # Checks if config for file exists in noise_config.json else create
+            self.find_noise_config()
 
 def cli_main():
     """Main function to run the CLI."""
